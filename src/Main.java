6544be18b0a8214366e955dab5b223141c0f8275
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-04 17:20:01 +0300

    task 5

--- src/Main.java
+++ src/Main.java
@@ -44,20 +44,50 @@ public class Main {
             } else System.out.println(year + " год не является високосным");
         }
 
-        //Задание 4:
+        //Задание 4
         {
             System.out.println("\nЗадание 4:");
             int deliveryDistance = 415;
             int daysForDelivery = 1;
             if (deliveryDistance < 20) {
-                System.out.println("Потребуется дней: "+daysForDelivery);
+                System.out.println("Потребуется дней: " + daysForDelivery);
             } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
                 daysForDelivery = daysForDelivery + 1;
-                System.out.println("Потребуется дней: "+daysForDelivery);
+                System.out.println("Потребуется дней: " + daysForDelivery);
             } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
                 daysForDelivery = daysForDelivery + 2;
-                System.out.println("Потребуется дней: "+daysForDelivery);
+                System.out.println("Потребуется дней: " + daysForDelivery);
             } else System.out.println("Расстояние для доставки слишком большое :(");
+        }
+
+        //Задание 5
+        {
+            System.out.println("\nЗадание 5:");
+            int monthNumber = 8;
+            switch (monthNumber) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("Это зимний месяц!");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Это весенний месяц!");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Это летний месяц!");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Это осенний месяц!");
+                    break;
+                default:
+                    System.out.println("Такого месяца не бывает.");
+            }
         }
     }
 }
