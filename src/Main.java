edb98b2f3caa9afc913aa2e5371eb2ea297c79fe
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-04 16:17:31 +0300

    task 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Задание 1
+        System.out.println("Задание 1:");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+}
