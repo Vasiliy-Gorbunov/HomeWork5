92ad0848462534eb27c8626cb0ecfff5fff538ac
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-04 16:23:47 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,32 @@ public class Main {
     public static void main(String[] args) {
 
         //Задание 1
-        System.out.println("Задание 1:");
-        int clientOS = 0;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке");
+        {
+            System.out.println("Задание 1:");
+            int clientOS = 0;
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+        //Задание 2
+        {
+            System.out.println("\nЗадание 2:");
+            int clientOS = 1;
+            int clientDeviceYear = 2013;
+            if (clientDeviceYear >= 2015) {
+                if (clientOS == 0) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+            } else if (clientOS == 0) {
+                System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+            }
         }
     }
 }
